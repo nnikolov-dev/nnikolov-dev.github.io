7c9ddcb427e94615d64491fcfabb8929a49ecf3e
Nikita Nikolov <nn00317@surrey.ac.uk>
2019-09-18 15:00:56 +0100

    Initial commit

---
+++ src/components/GitHub.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import moment from 'moment'
+
+function GitHub(props) {
+    const [data, setData] = useState(null)
+    const [limit, setLimit] = useState(5)
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async() => {
+        const result = await axios('https://api.github.com/users/nnikolov-dev/events')
+        setData(result.data)
+        console.log(result.data)
+    }
+
+    const incLimit = () => {
+        setLimit(limit + 5)
+    }
+
+    const getPayload = (type, payload) => {
+        switch(type) {
+            case 'CreateEvent':
+                return `Created a new ${payload.ref_type} ${payload.ref ? ` @ ${payload.ref}` : ''}` 
+            case 'GollumEvent':
+                    return `Updated the ${payload.pages[0].page_name} wiki page` 
+            case 'PushEvent':
+                return payload.commits[0].message
+            case 'ForkEvent':
+                return `Forked ${payload.forkee.name}`
+            case 'PullRequestEvent':
+                return `${payload.action} a pull request`
+            default:
+                return null
+        }
+    }
+
+    const EVENTS_ICONS = []
+    EVENTS_ICONS['CreateEvent'] = 'icon fa-plus'
+    EVENTS_ICONS['GollumEvent'] = 'icon fa-file-text-o'
+    EVENTS_ICONS['PushEvent'] = 'icon fa-github'
+    EVENTS_ICONS['ForkEvent'] = 'icon fa-code-fork'
+    EVENTS_ICONS['PullRequestEvent'] = 'icon fa-question-circle-o'
+
+    return(
+        <>
+            <table className="table-wrapper">
+                <tbody>
+                    {data ? data.slice(0, limit).map((activity) => (
+                        <tr>
+                            <td>
+                                <i className={EVENTS_ICONS[activity.type]}/>
+                            </td>
+                            <td>
+                                <a
+                                    href={`http://github.com/${activity.repo.name}`}
+                                    target="_blank" alt={`Visit ${activity.repo.name}'s GitHub page`}
+                                    rel="noopener noreferrer"
+                                >
+                                    {activity.repo.name}
+                                </a>
+                            </td>
+                            <td>
+                                {getPayload(activity.type, activity.payload)}
+                            </td>
+                            <td>
+                                {moment(activity.created_at).fromNow()}
+                            </td>
+                        </tr>
+                    )) : null}
+                </tbody>
+            </table>
+            {data ? <span id="table-footer" onClick={incLimit}>Load More</span> : null }
+        </>
+    )
+}
+
+export default GitHub
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
-// import Lightbox from 'react-images'
+import GitHub from '../components/GitHub'
 import Gallery from '../components/Gallery'
 
 import thumb01 from '../assets/images/thumbs/01.jpg'
@@ -31,8 +31,8 @@ const DEFAULT_IMAGES = [
 class HomeIndex extends React.Component {
 
     render() {
-        const siteTitle = "Gatsby Starter - Strata"
-        const siteDescription = "Site description"
+        const siteTitle = "nnikolov-dev"
+        const siteDescription = "The personal portfolio of Nikita Nikolov"
 
         return (
             <Layout>
@@ -44,18 +44,7 @@ class HomeIndex extends React.Component {
                 <div id="main">
 
                     <section id="one">
-                        <header className="major">
-                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
-                            magna sed accumsan arcu neque.</h2>
-                        </header>
-                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
-                        <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
-                    </section>
-
-                    <section id="two">
-                        <h2>Recent Work</h2>
+                        <h2>Projects</h2>
 
                         <Gallery images={DEFAULT_IMAGES.map(({ id, source, thumbnail, caption, description }) => ({
                             source,
@@ -64,42 +53,26 @@ class HomeIndex extends React.Component {
                             description
                         }))} />
 
-                        <ul className="actions">
-                            <li><a href="#" className="button">Full Portfolio</a></li>
-                        </ul>
+                    </section>
+
+                    <section id="two">
+                        <h2>GitHub Activity</h2>
+                        <GitHub/>
                     </section>
 
                     <section id="three">
                         <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
-                                <form method="post" action="#">
-                                    <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                    </div>
-                                </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
+                                <a href="#" className="button">View my CV</a>
                             </div>
                             <div className="4u 12u$(small)">
                                 <ul className="labeled-icons">
                                     <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        1234 Somewhere Rd.<br />
-                                        Nashville, TN 00000<br />
-                                        United States
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
-                                    </li>
-                                    <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">hello@untitled.tld</a>
+                                        nn00317@surrey.ac.uk<br />
+                                        University of Surrey<br />
+                                        United Kingdom
                                     </li>
                                 </ul>
                             </div>
